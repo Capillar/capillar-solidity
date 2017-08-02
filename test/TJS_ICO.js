babd89992ddeaf9886521b8fa53cd9f817dd38b3
ArietZ <iborisov@acconcept.ru>
2017-08-02 16:04:34 +0300

    Refoctoring: chai synthax

--- test/TJS_ICO.js
+++ test/TJS_ICO.js
@@ -3,7 +3,6 @@ var ICOControll = artifacts.require("./icoController.sol");
 var expect = require("chai").expect;
 
 // ============= Test suite for ICO functionality ==================
-// @using async + await
 // @using chai for parsing and assertion
 contract('ICO', function(accounts) 
 {
@@ -13,15 +12,14 @@ contract('ICO', function(accounts)
 		.then(function(instance) 
 			{  return instance.capDB.call();  })
 		.then(function(adr) 
-			{ assert.equal(adr.valueOf(), TokenCAP.address, "Address for CAP databse in controll is invalid"); })
+			{ expect(adr.valueOf()).to.equal(TokenCAP.address); })
 		.then(function()
 		{  
 			return TokenCAP.deployed()
 			.then(function(instance2) 
 				{  return instance2.owner.call(); })
 			.then(function(adr) 
-				{ assert.equal(adr.valueOf(), ICOControll.address, "Address for controll in database is invalid"); });
+				{ expect(adr.valueOf()).to.equal(ICOControll.address); });
 		});
 	});
-	// TODO: test ICO functions
 });
